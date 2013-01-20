55bf33969974590e5fa02dac7708ca59caf0c65f
Alex Hokanson <ingshtrom.developer@gmail.com>
2013-01-20 10:49:54 -0500

    Changed name to BlockCursorEverywhere

--- SublimeBlockCursor.py
+++ SublimeBlockCursor.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 
 
-class SublimeBlockCursor(sublime_plugin.EventListener):
+class BlockCursorEverywhere(sublime_plugin.EventListener):
     def view_is_widget(self, view):
         settings = view.settings()
         return bool(settings.get('is_widget'))
@@ -14,18 +14,18 @@ class SublimeBlockCursor(sublime_plugin.EventListener):
                 continue
             validRegions.append(sublime.Region(s.a, s.a + 1))
         if validRegions.__len__:
-            view.add_regions('SublimeBlockCursorListener', validRegions, 'block_cursor')
+            view.add_regions('BlockCursorListener', validRegions, 'block_cursor')
         else:
-            view.erase_regions('SublimeBlockCursorListener')
+            view.erase_regions('BlockCursorListener')
 
     def on_selection_modified(self, view):
         if view.settings().get('is_widget') or not("Vintage" in view.settings().get('ignored_packages') or view.settings().get('command_mode')):
-            view.erase_regions('SublimeBlockCursorListener')
+            view.erase_regions('BlockCursorListener')
             return
         self.show_block_cursor(view)
 
     def on_deactivated(self, view):
-        view.erase_regions('SublimeBlockCursorListener')
+        view.erase_regions('BlockCursorListener')
         view.settings().clear_on_change('command_mode')
         self.current_view = None
 
